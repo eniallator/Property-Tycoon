f9e09afcebfa186fcaa999b5e8d8215c3d28ff24
bikeboi <mikegithegi@gmail.com>
2020-02-18 23:43:30 +0000

    GameState documentation additions

--- app/src/core/game-state.ts
+++ app/src/core/game-state.ts
@@ -8,10 +8,28 @@
 import { Player } from './player'
 import { Property } from './tile'
 
-// Game State
+/**
+ * GamePhase represents current event the game is in:
+ * - `PLAYER_MOVE`: A player is moving around the board
+ * - `MAIN_MENU`: TODO 
+ * - `PAUSE_MENU`: TODO
+ * - `BUY_PROPERTY`: TODO
+ * - `AUCTION`: TODO
+ * - `PAY_RENT`: TODO
+ * - `PROPERTY_MANAGEMENT`: TODO
+ * - `END_GAME`: TODO
+ */
 enum GamePhase { PLAYER_MOVE }
 type Card = string
 
+/**
+ * GameState
+ * - `gamePhase`: Current phase the game is in
+ * - `players`: Collection of players in the game
+ * - `activePlayer`: Current active player
+ * - `properties`: TODO
+ * - `cards`: TODO
+ */
 interface GameState {
     gamePhase: GamePhase
     players: Array<Player>
