2e3f4da17d3dbf38d18819183053663302df0395
bikeboi <mikegithegi@gmail.com>
2020-02-23 19:03:49 +0000

    Added main file for game entrypoint

--- app/src/core/index.ts
+++ app/src/core/index.ts
@@ -2,10 +2,12 @@ import { Tile } from './tile'
 import { Player, PlayerM } from './player'
 import { State, StateM } from './state'
 import * as Cmd from './command'
+import { Core } from './core'
 
 export { 
     Tile, 
     Player, PlayerM,
     State, StateM,
-    Cmd
+    Cmd,
+    Core
 }
