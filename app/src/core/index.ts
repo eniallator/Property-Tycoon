348b2d174742b47f729b8f4e702d2d86b23271f1
bikeboi <mikegithegi@gmail.com>
2020-02-23 20:24:02 +0000

    Unit tests and fixes - State

--- app/src/core/index.ts
+++ app/src/core/index.ts
@@ -1,13 +1,13 @@
 import { Tile } from './tile'
 import { Player, Token, PlayerM } from './player'
-import { State, StateM } from './state'
+import { State, GamePhase, StateM } from './state'
 import * as Cmd from './command'
 import { Core } from './core'
 
 export { 
     Tile, 
     Player, Token, PlayerM,
-    State, StateM,
+    State, GamePhase, StateM,
     Cmd,
     Core
 }
--- app/test/state.test.js
+++ app/test/state.test.js
@@ -8,20 +8,18 @@ const { State, GamePhase, StateM } = require('../dist/core')
 
 // State initialization
 test('Initialize the game state', () => {
-    const s = StateM.createGameState()
+    const numTiles = 40
+    const s = StateM.createGameState(numTiles)
     
-    // Parameters
-    const initialState = GamePhase.PLAYER_MOVE // TODO: Should be Main Menu
-
     // Initial State
-    expect(s.gamePhase),toEqual(initialState)
+    expect(s.gamePhase).toEqual(GamePhase.PLAYER_MOVE)
 
     // No. players
-    // TODO: Initially game has no players
+    expect(s.players).toHaveLength(6)
 
     // Active Player
-    // TODO: Active player is first in collection
+    expect(s.activePlayer).toEqual(1)
 
     // Tiles
-    // TODO: Initially game has no properties
+    expect(s.tiles).toHaveLength(numTiles)
 })
