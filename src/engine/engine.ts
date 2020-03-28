9e3b127ec3f9f0aaad166d6b903037396bb9e252
bikeboi <mikegithegi@gmail.com>
2020-03-28 16:29:27 +0000

    Fixed error with renderer logging wrong source

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -30,7 +30,7 @@ class Engine {
         this.core = new Core()
 
         this.io.logInfo(
-            LogSource.CORE,
+            LogSource.RENDERER,
             "Initializing Renderer"
         )
         this.renderer = new Renderer(this.io)
