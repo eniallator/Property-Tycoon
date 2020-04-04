bfe449a25d5f7c255be9a406a38127b92948f0d5
cristiean <alexandru.cristiean@gmail.com>
2020-04-04 15:57:50 +0100

    some questions

--- src/io/io.ts
+++ src/io/io.ts
@@ -32,6 +32,7 @@ class IO {
      * @param logCmd Game command log flag - Default true
      */
     constructor(logSys: boolean = true, logCmd: boolean = true) { 
+        // TODO: @alexandru says: Why is this command hardcoded here? Can I remove it?
         this.command = {
             type: CommandType.ROLL,
             data: {
