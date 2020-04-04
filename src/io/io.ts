aad21a93e384e536ef211a501122403fa4dd7e61
cristiean <alexandru.cristiean@gmail.com>
2020-04-04 16:59:45 +0100

    remove placeholder

--- src/io/io.ts
+++ src/io/io.ts
@@ -31,13 +31,6 @@ class IO {
      * @param logCmd Game command log flag - Default true
      */
     constructor(logSys: boolean = true, logCmd: boolean = true) { 
-        // TODO: @alexandru says: Why is this command hardcoded here? Can I remove it?
-        this.command = {
-            type: CommandType.ROLL,
-            data: {
-                dice: [1, 1]
-            }
-        }
         this.state = StateM.initialiseGameState()
 
         this.logCmd = logCmd
