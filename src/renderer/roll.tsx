3753b88212218ab08016e6b5599a534fd975d529
bikeboi <mikegithegi@gmail.com>
2020-04-07 18:01:05 +0100

    Updated roll functionality to new API

--- src/renderer/roll.tsx
+++ src/renderer/roll.tsx
@@ -9,30 +9,34 @@
 import React, { Fragment, Component,MouseEvent } from "react";
 import ReactDOM from "react-dom";
 import { IO } from '../io/io';
-import { RollData,Command, CommandType } from "../game_data/command";
+import { CommandM } from "../game_data/command";
 import { SendProps } from "./props"
 
 class Roll extends Component<SendProps>{
     handleClick(event: MouseEvent) {
-        const n1 = Math.floor(Math.random() * 6) + 1
-        const n2 = Math.floor(Math.random() * 6) + 1
+      const n1 = Math.floor(Math.random() * 6) + 1
+      const n2 = Math.floor(Math.random() * 6) + 1
         
-        const rollData: RollData  = { dice: [n1, n2]  } as RollData;
-
-        const rollcmd: Command = {
-            type: CommandType.ROLL,
-            data: rollData
-        }
-
-        this.props.io.sendCommand(rollcmd)
+      // Hi guys. I've updated this to use the new API
+      // As you can see it's a lot simpler to utilize
+      // Here's the old code:
         
-      }
+      // const rollData: RollData  = { dice: [n1, n2]  } as RollData;
+
+      // const rollcmd: Command = {
+      //     type: CommandType.ROLL,
+      //     data: rollData
+      // }
+
+      const rollCmd = CommandM.movePlayer(n1 + n2)
+      this.props.io.sendCommand(rollCmd)
+    }
       
-      render() {
-        return <Fragment><button onClick={this.handleClick}> Roll Dice
-        </button> <div> num here</div> </Fragment>
+    render() {
+      return <Fragment><button onClick={this.handleClick}> Roll Dice
+      </button> <div> num here</div> </Fragment>
         
-      }
+    }
 
 
 
